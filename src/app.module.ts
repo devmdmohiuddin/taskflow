19ba69197d6214f41afdd4e6c948137d8080302a
devmdmohiuddin <devmdmohiuddin0241@gmail.com>
2026-04-25 23:26:37 +0600

    feat(database): prisma integration with user and refresh-token models

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { appConfig } from './config/app.config';
 import { jwtConfig } from './config/jwt.config';
 import { envValidationSchema } from './config/env.validation';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { envValidationSchema } from './config/env.validation';
         allowUnknown: true,
       },
     }),
+    DatabaseModule,
   ],
 })
 export class AppModule {}
